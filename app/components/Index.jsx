1254cb08d99629364cf709d5060fbe8958568ecf
kaina <ikomangkrisna40@gmail.com>
2024-03-05 09:40:26 +0800

    smooth

---
+++ app/components/Index.jsx
@@ -0,0 +1,11 @@
+'use client';
+import Image from "next/image";
+
+
+export default function index() {
+  return (
+    <div className="index">
+        
+    </div>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,17 @@
+'use client';
 import Image from "next/image";
-import './globals.css'
+import "./globals.css"
+import Index from "./components/Index";
 
+import LocomotiveScroll from 'locomotive-scroll';
 
+const scroll = new LocomotiveScroll();
 
 export default function Home() {
   return (
     <main className="main">
-
+      <Index/>
+      <p>infokan joki radiant mas</p>
     </main>
   );
 }
